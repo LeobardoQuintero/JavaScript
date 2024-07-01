76a7adf92fe76a706e014df91a4a44f92041ebe9
Leobardo Quintero <Leobardoquintero2002@gmail.com>
2024-06-30 23:52:26 -0600

    submit'

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+const friend = "BRUTUS"
+const shiftValue = 3;
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const firstLetter = friend[0];
+const index = alphabet.indexOf(firstLetter.toLowerCase());
+const newIndex = (index + shiftValue) % alphabetLength;
+const encryptedFirstLetter = alphabet[newIndex];
+const alphabetLength = alphabet.length;
+const encryptedMessage = "EUXWXV";
+const teaserMessage = encryptedMessage.slice(0, 3);
